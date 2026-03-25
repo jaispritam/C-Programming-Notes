8059a47702fa7dd8a3f37f5492a8c5d6f90e0a6e
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:29:57 +0530

    Update question_13.c

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_13.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_13.c
@@ -1,1 +1,17 @@
 
+2.13 Distance Between Two Points
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float x1,y1,x2,y2,d;
+
+    printf("Enter points: ");
+    scanf("%f %f %f %f",&x1,&y1,&x2,&y2);
+
+    d = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+
+    printf("Distance = %f", d);
+
+    return 0;
+}
