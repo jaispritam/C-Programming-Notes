899dcb4fb293ed3c9675d8325f0edbca9a8b5a5f
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 22:58:57 +0530

    Update question_8.c

---
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_8.c
@@ -0,0 +1,20 @@
+/*
+1.8 Three Points in Straight Line
+Concept
+
+3 points are collinear if slopes are equal
+OR area of triangle = 0
+
+Formula
+
+(x2 − x1)(y3 − y1) = (y2 − y1)(x3 − x1)
+
+Algorithm
+Start
+Input x1,y1,x2,y2,x3,y3
+If (x2-x1)(y3-y1) == (y2-y1)(x3-x1)
+→ Points are collinear
+Else → Not collinear
+Display result
+Stop
+*/
