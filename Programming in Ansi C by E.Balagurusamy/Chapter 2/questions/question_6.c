2e10540f8071f6f4f44f86ecfdbc56a83c704b82
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:12:01 +0530

    Update question_6.c

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_6.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_6.c
@@ -1,19 +1,1 @@
-/*
-1.6 Leap Year Check
 
-Rule
-Divisible by 4 → leap year
-BUT if divisible by 100 → not leap
-BUT if divisible by 400 → leap
-
-Algorithm
-Start
-Input year
-If (year % 400 == 0) → leap
-Else if (year % 100 == 0) → not leap
-Else if (year % 4 == 0) → leap
-Else → not leap
-Display result
-Stop
-
-*/
