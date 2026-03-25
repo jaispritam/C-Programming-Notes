9b436aa6d4a3877d2a14decdc856fab2bc855f90
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:28:08 +0530

    Update question_7.c

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_7.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_7.c
@@ -1,1 +1,20 @@
 
+2.7 Add & Sub using Functions
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    int a = 20, b = 10;
+
+    printf("20 + 10 = %d\n", add(a,b));
+    printf("20 - 10 = %d\n", sub(a,b));
+
+    return 0;
+}
