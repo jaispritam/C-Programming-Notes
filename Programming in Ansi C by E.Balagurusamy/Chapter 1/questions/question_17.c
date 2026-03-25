a1ecd2009585bdcfc0eabb0bb75bd949cd982881
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:15:54 +0530

    Update question_17.c

---
+++ Programming in Ansi C by E.Balagurusamy/Chapter 1/questions/question_17.c
@@ -0,0 +1,19 @@
+/*
+1.17 Series (1² + 3² + 5² + … N terms)
+Pattern
+
+Odd numbers: 1,3,5,…
+Term = (2i − 1)
+
+Algorithm
+Start
+Input n
+sum = 0
+For i = 1 to n:
+term = (2×i − 1)²
+sum += term
+Display sum
+Stop
+
+
+*/
