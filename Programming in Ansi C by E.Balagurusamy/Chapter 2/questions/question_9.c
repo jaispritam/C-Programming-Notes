69895bd7731fe08444c38da6362adc04fba782b9
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:13:38 +0530

    Update question_9.c

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_9.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_9.c
@@ -1,18 +1,1 @@
-/*
-1.9 Age Calculation (Years, Months, Days)
-Algorithm (basic logic)
-Start
-Input birth date (d1,m1,y1)
-Input current date (d2,m2,y2)
-If d2 < d1:
-d2 += 30
-m2 -= 1
-days = d2 − d1
-If m2 < m1:
-m2 += 12
-y2 -= 1
-months = m2 − m1
-years = y2 − y1
-Display age
-Stop
-*/
+
