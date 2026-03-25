945b1ed85af25a7f62bbcb8eccda747a32b546ec
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:26:25 +0530

    Implement 3D bar graphics function example
    Added a simple 3D bar graphics function example using Turbo C.

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_2.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_2.c
@@ -1,1 +1,20 @@
 
+2.2 3D Bar (Graphics Function)
+
+👉 This uses graphics.h (Turbo C / old compiler)
+
+#include <graphics.h>
+
+int main() {
+    int gd = DETECT, gm;
+
+    initgraph(&gd, &gm, "");
+
+    bar3d(100, 100, 200, 200, 20, 1);
+
+    getch();
+    closegraph();
+    return 0;
+}
+
+⚠️ Reality: This will NOT run in modern VS Code. Only Turbo C / old setup.
