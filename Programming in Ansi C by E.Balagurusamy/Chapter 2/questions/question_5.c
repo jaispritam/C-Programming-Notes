dc34d500e4dfedeb5997bb300d607aa38299c7c8
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:11:27 +0530

    Update question_5.c

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_5.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_5.c
@@ -1,18 +1,1 @@
-/*
-1.5 Sum of Digits (3-digit number)
-Logic
 
-Example: 123 → 1 + 2 + 3 = 6
-
-Algorithm
-Start
-Input number n
-d1 = n % 10
-n = n / 10
-d2 = n % 10
-n = n / 10
-d3 = n % 10
-sum = d1 + d2 + d3
-Display sum
-Stop
-*/
