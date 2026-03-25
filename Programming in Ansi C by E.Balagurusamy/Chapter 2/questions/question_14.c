b0b865b8a330fd4e908b7acef928131160b1032a
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:30:25 +0530

    Implement circle area and perimeter calculation

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_14.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_14.c
@@ -1,1 +1,22 @@
 
+2.14 Circle (Given Center and Point)
+
+👉 Radius = distance from center to point
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float x=4, y=5;
+    float r, area, perimeter;
+
+    r = sqrt(x*x + y*y);
+
+    area = 3.14 * r * r;
+    perimeter = 2 * 3.14 * r;
+
+    printf("Area = %f\n", area);
+    printf("Perimeter = %f\n", perimeter);
+
+    return 0;
+}
