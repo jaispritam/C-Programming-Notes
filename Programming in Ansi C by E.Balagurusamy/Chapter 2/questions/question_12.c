78acaa13c0279ed2d06d6930dcf13a3a972583a3
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:29:41 +0530

    Add simple calculator program in C

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_12.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_12.c
@@ -1,1 +1,16 @@
+2.12 Simple Calculator
+#include <stdio.h>
 
+int main() {
+    int x, y;
+
+    printf("Enter x and y: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Sum = %d\n", x+y);
+    printf("Difference = %d\n", x-y);
+    printf("Product = %d\n", x*y);
+    printf("Division = %d\n", x/y);
+
+    return 0;
+}
