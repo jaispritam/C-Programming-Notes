376f05c1e3bca0a528cac829c66eb686e65cb989
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:27:40 +0530

    Add program to display current system date

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_6.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_6.c
@@ -1,1 +1,19 @@
 
+2.6 Current System Date
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t t;
+    struct tm *current;
+
+    time(&t);
+    current = localtime(&t);
+
+    printf("Date: %02d-%02d-%04d\n",
+           current->tm_mday,
+           current->tm_mon + 1,
+           current->tm_year + 1900);
+
+    return 0;
+}
