110dc926417670cbbff46ff42193004f82614d39
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:27:20 +0530

    Implement speed calculation based on distance and time

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_5.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_5.c
@@ -1,1 +1,19 @@
 
+2.5 Speed = Distance / Time
+#include <stdio.h>
+
+int main() {
+    float distance, time, speed;
+
+    printf("Enter distance and time: ");
+    scanf("%f %f", &distance, &time);
+
+    if(time == 0) {
+        printf("Error: Time cannot be zero\n");
+    } else {
+        speed = distance / time;
+        printf("Speed = %f\n", speed);
+    }
+
+    return 0;
+}
