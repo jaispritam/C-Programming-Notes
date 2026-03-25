22d5e4c338ae3426e5e45907608a75693cdc6962
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:29:24 +0530

    Implement Heron's formula to calculate triangle area

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_11.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_11.c
@@ -1,1 +1,16 @@
 
+2.11 Area of Triangle (Heron’s Formula)
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a=3, b=4, c=5;
+    float s, area;
+
+    s = (a+b+c)/2;
+    area = sqrt(s*(s-a)*(s-b)*(s-c));
+
+    printf("Area = %f", area);
+
+    return 0;
+}
