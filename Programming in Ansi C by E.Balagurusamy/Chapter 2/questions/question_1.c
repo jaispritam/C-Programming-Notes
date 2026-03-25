62e5ed9766b8b2f0344ac3a36191951057973226
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:25:26 +0530

    Update question_1.c

---
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_1.c
@@ -0,0 +1,14 @@
+2.1 Equation of Line (ax + by = c)
+
+👉 Given: a = 5, b = 8, c = 18
+
+C Program
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 8, c = 18;
+
+    printf("Equation: %dx + %dy = %d\n", a, b, c);
+
+    return 0;
+}
