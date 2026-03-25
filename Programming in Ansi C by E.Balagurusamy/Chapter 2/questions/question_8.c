9bbae84993ab20692374afe69740e612ba2c6a71
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:28:29 +0530

    Add formatted address output in question_8.c

--- Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_8.c
+++ Programming in Ansi C by E.Balagurusamy/Chapter 2/questions/question_8.c
@@ -1,1 +1,11 @@
 
+2.8 Address with Border
+#include <stdio.h>
+
+int main() {
+    printf("********************\n");
+    printf("*   Pritam Roy     *\n");
+    printf("*   Kolkata, India *\n");
+    printf("********************\n");
+    return 0;
+}
