c688244bdf6dc1e0a6ceae22046e2e3fe1d755e5
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:15:03 +0530

    Implement sum of prime numbers algorithm
    Added algorithm to calculate the sum of prime numbers from 1 to 200.

---
+++ Programming in Ansi C by E.Balagurusamy/Chapter 1/questions/question_15.c
@@ -0,0 +1,18 @@
+/*
+1.15 Sum of Prime Numbers (1 to 200)
+Algorithm
+Start
+sum = 0
+For num = 2 to 200:
+flag = 1
+For i = 2 to num/2:
+If num % i == 0:
+flag = 0
+break
+If flag == 1:
+sum += num
+Display sum
+Stop
+
+
+*/
