7b5666fb224f50052df6c1a431af9c261c18ce44
Pritam Jaiswal <pritamjais006@gmail.com>
2026-03-25 23:15:34 +0530

    Add algorithm comment for displaying digits in words
    Added a comment block explaining the algorithm to display digits in words.

---
+++ Programming in Ansi C by E.Balagurusamy/Chapter 1/questions/question_16.c
@@ -0,0 +1,20 @@
+/*
+1.16 Display Digits in Words
+Example
+
+123 → One Two Three
+
+Algorithm
+Start
+Input n
+Reverse number (to maintain order)
+While n > 0:
+digit = n % 10
+Print word of digit
+n = n / 10
+Stop
+
+(Word mapping: 0→Zero, 1→One, etc.)
+
+
+*/
